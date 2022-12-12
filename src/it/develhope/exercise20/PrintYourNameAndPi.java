f6f992d13500b2945d01c53db220bb40d8836e5b
Jason Di Bartolo <jasondibartolo98@gmail.com>
2022-12-13 00:20:36 +0100

    Initial Commit

---
+++ src/it/develhope/exercise20/PrintYourNameAndPi.java
@@ -0,0 +1,8 @@
+package it.develhope.exercise20;
+
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name = "Jason"; double numberPi = 3.141590;
+        System.out.printf("%s %nThe number is: %f", name, numberPi);
+    }
+}
